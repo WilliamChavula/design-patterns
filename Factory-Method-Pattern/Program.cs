9abf1d0869a8daa6b456bd3034c641fabe5796f5
williamchavula <rumbani@gmail.com>
2021-08-04 10:21:41 -0500

    add client code to create IAnimal objects.

--- Factory-Method-Pattern/Program.cs
+++ Factory-Method-Pattern/Program.cs
@@ -6,7 +6,23 @@ namespace Factory_Method_Pattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("*** Factory Method Pattern Demo. ***\n");
+
+            // creating a TigerFactory
+            AnimalFactory tigerFactory = new TigerFactory();
+
+            // creating a tiger using the factory method
+            IAnimal tiger = tigerFactory.CreateAnimal();
+            tiger.AboutMe();
+
+            // creating a DogFactory
+            AnimalFactory dogFactory = new DogFactory();
+
+            // creating a Dog using the factory method
+            IAnimal dog = dogFactory.CreateAnimal();
+            dog.AboutMe();
+
+            Console.ReadKey();
         }
     }
 }
