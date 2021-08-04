4279e7fa7ca252e411497020a3b92c7e4f35abf9
williamchavula <rumbani@gmail.com>
2021-08-04 14:46:47 -0500

    create concrete PetAnimal factory to create pet dogs and tigers.

---
+++ Abstract-Factory-Pattern/PetAnimalFactory.cs
@@ -0,0 +1,15 @@
+namespace Abstract_Factory_Pattern
+{
+    public class PetAnimalFactory : IAnimalFactory
+    {
+        public IDog GetDog()
+        {
+            return new PetDog();
+        }
+
+        public ITiger GetTiger()
+        {
+            return new PetTiger();
+        }
+    }
+}
