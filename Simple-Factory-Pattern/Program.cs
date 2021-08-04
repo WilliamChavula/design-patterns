fc83f3cfe252cf1da10b0cbc5987f626cf5e1cae
williamchavula <rumbani@gmail.com>
2021-08-03 21:29:35 -0500

    implement code using Simple Factory pattern.

--- Simple-Factory-Pattern/Program.cs
+++ Simple-Factory-Pattern/Program.cs
@@ -6,7 +6,23 @@ namespace Simple_Factory_Pattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("*** Simple Factory Pattern demo ***\n");
+            IAnimal preferredType = null;
+            SimpleFactory simpleFactory = new SimpleFactory();
+
+            #region The code region that can cary based on users preferences
+            /*
+             * Since this part may vary, we're moving the
+             * part to CreateAnimal() in SimpleFactory class.
+            */
+            preferredType = simpleFactory.CreateAnimal();
+            #endregion
+
+            #region The codes that do not change frequently.
+            preferredType.AboutMe();
+            #endregion
+
+            Console.ReadKey();
         }
     }
 }
