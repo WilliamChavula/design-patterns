61c23ccabf4835da59335fdd1dd61678090c8b43
williamchavula <rumbani@gmail.com>
2021-08-04 14:46:04 -0500

    create concrete factory that implements IAnimalFactory; it creates wild dogs and tigers.

---
+++ Abstract-Factory-Pattern/WildAnimalFactory.cs
@@ -0,0 +1,15 @@
+namespace Abstract_Factory_Pattern
+{
+    public class WildAnimalFactory : IAnimalFactory
+    {
+        public IDog GetDog()
+        {
+            return new WildDog();
+        }
+
+        public ITiger GetTiger()
+        {
+            return new WildTiger();
+        }
+    }
+}
