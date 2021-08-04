3d24bdedae44ff18ddd34492e458ecbf690fe76b
williamchavula <rumbani@gmail.com>
2021-08-04 14:50:56 -0500

    create PetAnimals and WildAnimals in client code using the AbstractFactoryPattern.

--- Abstract-Factory-Pattern/Program.cs
+++ Abstract-Factory-Pattern/Program.cs
@@ -6,7 +6,32 @@ namespace Abstract_Factory_Pattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***Abstract Factory Pattern Demo.***\n");
+
+            // Making Wild Dog and Wild Tiger through WildAnimalFactory.
+            // Here's the difference with the Factory Method Patter. 
+            // We are not new-ing up an instance of a Factory. That is abstracted away
+            IAnimalFactory animalFactory = FactoryProvider.GetAnimalFactory("wild");
+            IDog dog = animalFactory.GetDog();
+            ITiger tiger = animalFactory.GetTiger();
+            dog.AboutMe();
+            Console.WriteLine();
+            tiger.AboutMe();
+            Console.WriteLine();
+
+            Console.WriteLine("*************************");
+            Console.WriteLine();
+
+            // Making a pet dog and pet tiger though the PetAnimalFactory now.
+            animalFactory = FactoryProvider.GetAnimalFactory("pet");
+            dog = animalFactory.GetDog();
+            tiger = animalFactory.GetTiger();
+            
+            dog.AboutMe();
+            Console.WriteLine();
+            tiger.AboutMe();
+
+            Console.ReadKey();
         }
     }
 }
